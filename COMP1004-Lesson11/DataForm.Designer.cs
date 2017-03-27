cc676e8215e3a371fc09932263324e73429367cc
caitl <caitl@LAPTOP-61IVM83V>
2017-03-27 12:38:39 -0400

    Add project files.

---
+++ COMP1004-Lesson11/DataForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace COMP1004_Lesson11
+{
+    partial class DataForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsersDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.UsersDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UsersDataGridView
+            // 
+            this.UsersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UsersDataGridView.Location = new System.Drawing.Point(23, 30);
+            this.UsersDataGridView.Name = "UsersDataGridView";
+            this.UsersDataGridView.Size = new System.Drawing.Size(342, 181);
+            this.UsersDataGridView.TabIndex = 0;
+            // 
+            // DataForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(396, 227);
+            this.Controls.Add(this.UsersDataGridView);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "DataForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Form";
+            ((System.ComponentModel.ISupportInitialize)(this.UsersDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView UsersDataGridView;
+    }
+}
+
---
+++ COMP1004-Lesson11/DataForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_Lesson11
+{
+    public partial class DataForm : Form
+    {
+        //SchoolContext SchoolDB = new SchoolContext();
+
+        public DataForm()
+        {
+            InitializeComponent();
+
+            /*
+             * try
+             * {
+            selects all the users in the Users table of the Name DB
+             var UsersList = (from user in NamesDB.Users
+                                Select user).FirstOrDefault();
+
+                UsersDataGridView.DataSource = UsersList;
+
+                UsersDataGridView.
+               }
+               catch (Exception exception)
+               {
+                Debug.WriteLine(.exception);
+               }
+            
+            foreach (var user in UserList)
+            {
+                Debug.WriteLine(user.FirstName);
+            }
+           
+             var CourseList = (from course in SchoolDb.Courses
+                                select course).ToList();
+
+            foreach(var course in CourseList)
+            {
+                Debug.WriteLine(course.CourseShortdescription);
+            }
+             */
+        }
+
+        private void UsersDataGridView_SelectionChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            var rowindex = UsersDataGridView.CurrentRow.Index;
+            var currentId = UsersDataGridView.Rows[rowindex].Cells[0].Value;
+
+            try
+            {
+                var SelectedName = (from name in NamesDb.Users
+                                    where name.Id == currentId
+                                    select name).FirstOrDefault();
+
+                Debug.WriteLine(selectedName.FirstName);
+            }
+        }
+    }
+}
